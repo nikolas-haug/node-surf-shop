028ccf7c0a8c30c06a0a8b9738287b9963c239b2
Nikolas Haug <nikolas.haug@futurenet.com>
2019-05-02 13:30:03 -0500

    add passport and db

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,15 @@ const User = require('../models/user');
 
 module.exports = {
     postRegister(req, res, next) {
-        res.send('POST REGISTER');
+        User.register(new User({ username: req.body.username }), req.body.password, (err) => {
+            if (err) {
+                console.log('error while user register!', err);
+                return next(err);
+            }
+
+            console.log('user registered!');
+
+            res.redirect('/');
+        });
     }
 }
