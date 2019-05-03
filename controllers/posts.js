1f7e843386743adb2b53094c252cbbc5dd7abae9
Nikolas Haug <nikolas.haug@futurenet.com>
2019-05-03 09:50:32 -0500

    add first post  controllers

---
+++ controllers/posts.js
@@ -0,0 +1,10 @@
+const Post = require('../models/post');
+
+module.exports = {
+
+    async getPosts(req, res, next) {
+        let posts = await Post.find({});
+        res.render('posts/index', { posts });
+    }
+
+}
