36354db6c65da7b1ee2f3b5bbd723f6d65a3f9fe
nikolas-haug <nikohaug21@gmail.com>
2019-05-08 13:41:57 -0500

    add map to page with markers

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,6 @@
 const Post = require('../models/post');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const geocodingClient = mbxGeocoding({ accessToken: process.env.MAPBOX_TOKEN });
 const cloudinary = require('cloudinary');
 cloudinary.config({
     cloud_name: 'dnvibrbhq',
@@ -29,6 +31,13 @@ module.exports = {
                 public_id: image.public_id
             });
         }
+        let response = await geocodingClient
+            .forwardGeocode({
+                query: req.body.post.location,
+                limit: 1
+            })
+            .send();
+        req.body.post.coordinates = response.body.features[0].geometry.coordinates;
         let post = await Post.create(req.body.post);
         res.redirect(`/posts/${post.id}`);
     },
@@ -50,16 +59,16 @@ module.exports = {
         // find the post by id
         let post = await Post.findById(req.params.id);
         // check if there's any images for deletion
-        if(req.body.deleteImages && req.body.deleteImages.length) {
+        if (req.body.deleteImages && req.body.deleteImages.length) {
             // assign deleteImages from req.body to its own variable
             let deleteImages = req.body.deleteImages;
             // loop over deleteImages
-            for(const public_id of deleteImages) {
+            for (const public_id of deleteImages) {
                 // delete images froom cloudinary
                 await cloudinary.v2.uploader.destroy(public_id);
                 // delete images from post.images
-                for(const image of post.images) {
-                    if(image.public_id === public_id) {
+                for (const image of post.images) {
+                    if (image.public_id === public_id) {
                         let index = post.images.indexOf(image);
                         post.images.splice(index, 1);
                     }
@@ -67,7 +76,7 @@ module.exports = {
             }
         }
         // check if there are any new images for upload
-        if(req.files) {
+        if (req.files) {
             // upload images
             for (const file of req.files) {
                 let image = await cloudinary.v2.uploader.upload(file.path);
@@ -92,7 +101,7 @@ module.exports = {
     // Posts Destroy
     async postDestroy(req, res, next) {
         let post = await Post.findById(req.params.id);
-        for(const image of post.images) {
+        for (const image of post.images) {
             await cloudinary.v2.uploader.destroy(image.public_id);
         }
         await post.remove();
